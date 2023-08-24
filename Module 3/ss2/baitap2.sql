715e6fa4d7aec73d47bde638c5949c4878cfa6e7
TRINHLE2002 <ledangtrinh552002@gmail.com>
2023-08-24 10:05:04 +0700

    them

---
+++ Module 3/ss2/baitap2.sql
@@ -0,0 +1,32 @@
+create database if not exists ss2_bt2;
+use ss2_bt2;
+
+create table customer(
+cID int primary key not null,
+cName varchar(50),
+cAge int
+);
+
+create table `order`(
+oID int primary key not null,
+cID int,
+oDate date,
+oTotalPrice float
+);
+
+alter table `order` add foreign key (cID) references customer(cID);
+
+create table product(
+pID int primary key not null,
+pName varchar(50),
+pPice float
+);
+
+create table orderDetail(
+oID int,
+pID int,
+odQTY varchar(50),
+primary key(oID, pID),
+foreign key (oID) references `order`(oID),
+foreign key (pID) references product(pID)
+);
